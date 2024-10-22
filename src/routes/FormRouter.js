7ecaa211c306dad02514a15ba7c409db3e050c60
Suvro Biswas <shuvo.249963@gmail.com>
2024-10-23 01:13:16 +0600

    add Formservices and use it in FormRoute

--- src/routes/FormRouter.js
+++ src/routes/FormRouter.js
@@ -1,17 +1,18 @@
 const router = require('express').Router()
-const {createForm, formsGet, getFormById, deleteForm, editForm, allResponses, submitResponse, getResponse} = require('../controllers/form')
-const {validate} = require("../middlewares/validator");
-const formSchemas = require("../validations/formValidation");
 
-router.route("/").post( validate(formSchemas.createFormSchema),createForm);
-router.route("/").get(formsGet);
-router.route("/:formId").get(validate(formSchemas.getFormById),getFormById);
-router.route("/:formId/:userId").delete(validate(formSchemas.deleteForm),deleteForm);
-router.route("/:formId").put( validate(formSchemas.editFormSchema), editForm);
-router.route("/responses").post(submitResponse);
+const {createForm, formsGet, getFormById, deleteForm, editForm, getAllFormsOfUser, allResponses, submitResponse, getResponse} = require('../services/FormService')
+
+router.route("/create").post(createForm);
+router.route("/forms").get(formsGet);
+router.route("/form/:formId").get(getFormById);
+router.route("/deleteform/:formId/:userId").delete(deleteForm);
+router.route("/editform").put(editForm);
+router.route("/getuserforms/:userId").get(getAllFormsOfUser);
+
+router.route("/addresponse").post(submitResponse);
 router.route("/responses").get(allResponses);
 
-router.route("/:formId/responses").get(getResponse);
+router.route("/getresponse/:formId").get(getResponse);
 
 
 module.exports = router;
