e2b3d2e4a39ef010e12cc865ae7d52e0b2054eff
Suvro Biswas <shuvo.249963@gmail.com>
2024-10-23 03:34:06 +0600

    refactor route path, update form service formValidation user and form controller

--- src/controllers/form.js
+++ src/controllers/form.js
@@ -1,20 +1,73 @@
-const router = require('express').Router()
-const {createForm, formsGet, getFormById, deleteForm, editForm, getAllFormsOfUser, allResponses, submitResponse, getResponse} = require('../controllers/form')
-const {validate} = require("../middlewares/validator");
-const formSchemas = require("../validations/formValidation");
+const formService = require("../services/FormService");
+const userService = require("../services/UserService");
+const {ApiError} = require("../utils/ApiError");
 
-router.route("/create").post( validate(formSchemas.createFormSchema),createForm);
-router.route("/forms").get(formsGet);
-router.route("/form/:formId").get(validate(formSchemas.getFormById),getFormById);
-router.route("/deleteform/:formId/:userId").delete(validate(formSchemas.deleteForm),deleteForm);
-router.route("/editform").put(editForm);
-router.route("/getuserforms/:userId").get(getAllFormsOfUser);
+module.exports = {
+    formsGet : async(req,res)=>{
+        const forms = await formService.getAllForms();
+        return res.status(200).json(forms);    
+    },
 
-router.route("/addresponse").post(submitResponse);
-router.route("/responses").get(allResponses);
+    createForm: async(req,res)=>{     
 
-router.route("/getresponse/:formId").get(getResponse);
+        const formData = {
+        createdBy : req.body.createdBy,
+        name: req.body.name,
+        description: req.body.description
+        }
 
+        const form = await formService.createForm(formData);
+        return res.status(201).json(form);
+    },
 
+    getFormById: async(req, res)=>{
+            var formId = req.params.formId;
+            const form = await formService.getFormById(formId);
+            if(!form) {
+                throw new ApiError(404, "Form not found");
+            }
+            return res.status(200).json(form);
+    },
+    
+    deleteForm: async(req, res)=> {
+            const formId = req.params.formId;
+            const userId = req.params.userId;
+          
+            const form = await formService.getFormById(formId);
 
-module.exports = router;
+            if(form != null && form.createdBy == userId) {
+                await formService.deleteFormById(formId);
+                return res.status(200).json(null);
+            }
+            
+            return res.status(400).json(null);
+    },
+
+    editForm : async(req, res)=>{
+            var  formId =  req.params.formId;
+            var data = req.body;
+            await formService.updateFormById(formId, data);
+            return res.status(200).json(data);
+           
+    },
+
+    submitResponse : async(req, res)=> {
+            const formId = req.body.formId;
+            const data = {
+                userId: req.body.userId,
+                response: req.body.response
+            };
+            const formResponse = await formService.saveFormResponse(formId, data);
+            return res.status(200).json(formResponse)
+    },
+
+    allResponses : async(req,res)=>{
+        const responses = await formService.getAllFormResponses();
+        return res.status(200).json(responses);
+    },
+
+    getResponse: async(req, res)=>{
+        const responses = await formService.getFromResponsesByFormId();
+        return res.status(200).json(responses);
+    }
+}
