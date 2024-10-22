58eac42a740585d5598a1247e07e19b7dd3928fe
Suvro Biswas <shuvo.249963@gmail.com>
2024-10-23 02:36:17 +0600

    add formValidation and udpate form service

--- src/routes/FormRouter.js
+++ src/routes/FormRouter.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
-
 const {createForm, formsGet, getFormById, deleteForm, editForm, getAllFormsOfUser, allResponses, submitResponse, getResponse} = require('../services/FormService')
+const {validate} = require("../middlewares/validator");
+const formSchemas = require("../validations/formValidation");
 
-router.route("/create").post(createForm);
+router.route("/create").post( validate(formSchemas.createFormSchema),createForm);
 router.route("/forms").get(formsGet);
-router.route("/form/:formId").get(getFormById);
-router.route("/deleteform/:formId/:userId").delete(deleteForm);
+router.route("/form/:formId").get(validate(formSchemas.getFormById),getFormById);
+router.route("/deleteform/:formId/:userId").delete(validate(formSchemas.deleteForm),deleteForm);
 router.route("/editform").put(editForm);
 router.route("/getuserforms/:userId").get(getAllFormsOfUser);
 
@@ -13,6 +14,7 @@ router.route("/addresponse").post(submitResponse);
 router.route("/responses").get(allResponses);
 
 router.route("/getresponse/:formId").get(getResponse);
+
 
 
 module.exports = router;
